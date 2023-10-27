5244c42b6211ee715949f71c5169beab1ebb8970
Bjorn Agbalog <bjornlouie98@gmail.com>
2023-10-27 23:55:06 +0200

    corrected some excercise

--- Network Request- POST/exercise.js
+++ Network Request- POST/exercise.js
@@ -1,25 +1,38 @@
-async function createPost(){
 
-  const post = {
-    title: title,
-    completed: completed,
-  }
+  const form = document.querySelector('form');
+  const titleInput = document.getElementById('todo-title');
+  const completedCheckbox = document.getElementById('todo-completed');
 
-  try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts',{
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(post)
-    });
-    const data = await response.json();
-    console.log(data)
+  form.addEventListener('submit', async function (event) {
+    event.preventDefault();
 
-  } catch (error) {
-    
-    console.log(error)
+    const title = titleInput.value;
+    const completed = completedCheckbox.checked;
 
-  }
-}
-createPost()
+    const postObject = {
+      title: title,
+      completed: completed
+    };
+
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postObject)
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('New post created:', data);
+      } else {
+        console.log('Failed to create a new post.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  });
+
